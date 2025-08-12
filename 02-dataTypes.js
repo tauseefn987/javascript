f490771ec5d0a159967362df158528c9786277dd
tauseefn987 <tauseef780978@gmail.com>
2025-08-12 18:04:37 +0000

    dataTypes

---
+++ 02-dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3)  we are using nodejs, not brower
+
+let name = "Tauseef"  
+let age = 23  
+let isloggedIn = false  
+let state = null
+
+
+//number =>     2 to power 53
+// bigint
+// string =>     ""  ''
+// boolean =>   true/false
+// null =>      standlone value (empty khali)
+// undefined => (variable to define hai lkin value define nhi kiya hai)
+// symbol =>    unique
+
+
+
+// object
+
+console.log(typeof "tauseef")      //steing
+console.log(typeof age)           // number
+console.log(typeof null)         // object
+console.log(typeof undefined)   // undefined

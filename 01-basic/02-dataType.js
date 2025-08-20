ac369fac0d76204573f1c8e5b76904e393aaf3cc
tauseefn987 <tauseef780978@gmail.com>
2025-08-20 16:09:17 +0000

    dataType done

--- 01-basic/02-dataType.js
+++ 01-basic/02-dataType.js
@@ -1,3 +1,37 @@
+/* primitive or non primitive data types
+jis basic pr data ko memory mei store kiya jata hai or access kiya jata 
+ho us basic pr data ka 2 category ha primitive or non primitive */
+
+// primitive 
+
+// 7 types : string, number, boolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 345683487357934n
+
+
+// JavaScript is a dynamic language and not static,
+
+
+// reference (Non primitive)  
+
+// Array, Objects, Functions
+
+const heros = ["ironman"]
+
+// ***************     ***********************
+
 "use strict"; // treat all js code as newer version
 
 // alert(3 + 3)  we are using nodejs, not brower
